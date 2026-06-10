940e3fccf549158029d929fa158c29b2f85b92d1
hefxpzwk <hefxpzwk@gmail.com>
2026-06-10 11:59:37 +0900

    chapter09.js

---
+++ section01/chapter09.js
@@ -0,0 +1,15 @@
+//if(){}else if(){}else {}
+
+let animal = "dog";
+switch (animal) {
+  case "cat": {
+    console.log("고양이");
+    break;
+  }
+  case "dog": {
+    console.log("asdf");
+  }
+  default: {
+    console.log("왔더빡");
+  }
+}
