a95db44e1c00cbedbf70fd9c5f905f541a168f2c
hefxpzwk <hefxpzwk@gmail.com>
2026-06-10 14:03:54 +0900

    chapter14

---
+++ section01/chapter13.js
@@ -0,0 +1,41 @@
+//콜벡 함수
+
+function main(value) {
+  console.log(value);
+  value();
+}
+
+function sub() {
+  console.log("I am sub");
+}
+
+main(sub);
+main(() => {
+  console.log("I am fuck");
+});
+
+//콜백함수 활용
+function repeat(count) {
+  for (let idx = 1; idx <= count; idx++) {
+    console.log(idx);
+  }
+}
+function repeatDouble(count) {
+  for (let idx = 1; idx <= count; idx++) {
+    console.log(idx * 2);
+  }
+}
+repeat(5);
+repeatDouble(5);
+
+function repeatPerfact(count, callback) {
+  for (let idx = 1; idx <= count; idx++) {
+    callback(idx);
+  }
+}
+repeatPerfact(5, function (idx) {
+  console.log(idx);
+});
+repeatPerfact(5, (idx) => {
+  console.log(idx);
+});
